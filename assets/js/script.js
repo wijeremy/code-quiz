a9432f3221904291c9b71079c8af1b71551eba6f
wijeremy <jeremydavwilliams@gmail.com>
2021-05-25 14:57:14 -0500

    okay, this is pretty cool. I made a function that shuffles an array. okay, that's really all i've been doing the last hour or so, but it's so cool.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,39 +2,55 @@ var display = document.getElementById("display");
 var start = document.getElementById("start");
 
 var timeLeft = 30;
+var queue = 0;
 
-const question1 = [
-    "The condition of an if statement is enclosed in ...",
-    ["squre braces", false],
-    ["curly braces", false],
-    ["parentheses", true],
-    ["quotation marks", false]
+var questionWheel = [
+    [
+        "The condition of an if statement is enclosed in ...",
+        ["squre braces", false],
+        ["curly braces", false],
+        ["parentheses", true],
+        ["quotation marks", false]
+    ],
+    [
+        "In an object, parameters are separated by ...",
+        ["a comma", true],
+        ["a semi-colon", false],
+        ["a slash", false],
+        ["a line break", false],
+    ]
 ];
 
-const question2 = [
-    "In an object, parameters are separated by ...",
-    ["a comma", true],
-    ["a semi-colon", false],
-    ["a slash", false],
-    ["a line break", false],
-];
+function shuffleArray(array) {
+    var oldArray = [];
+    for (i=0;i<array.length;i++){
+        oldArray.push(array[i])
+    }
+    var newArray = [];
+    for (var i = 0; i<array.length; i++){
+        var rand = Math.floor(Math.random() * (oldArray.length));
+        newArray.push(oldArray[rand]);
+        oldArray.splice(rand,1);
+    }
+    return newArray;
+}
 
 function runGame(){
+    //reset display
     display.innerHTML = "";
     //display question
     var question = document.createElement("h2");
-    question.textContent = question1[0];
+    question.textContent = questionWheel[queue][0];
     display.appendChild(question);
-    //display answer buttons
-    for (var i = 1; i < question1.length; i++) {
+    //display answer buttons; we start at index 1 because index 0 is the question
+    for (var i = 1; i < questionWheel[queue].length; i++) {
         var answer = document.createElement("button");
-        answer.textContent = question1[i][0];
-        answer.setAttribute("data-key", question1[i][1]);
+        answer.textContent = questionWheel[queue][i][0];
+        answer.setAttribute("data-key", questionWheel[queue][i][1]);
         display.appendChild(answer);
         answer.addEventListener("click", function(){
-            console.log(this.getAttribute("data-key"));
             if (this.getAttribute("data-key") == "true"){
-                console.log("you win")
+                console.log("hurray");
             }
         });
     } 
