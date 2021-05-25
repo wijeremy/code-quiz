3a4b36fe5e6eaeaca8563286740e866da6a99b94
wijeremy <jeremydavwilliams@gmail.com>
2021-05-25 15:53:17 -0500

    oh baby now we're cooking with javascript. I just converted my game to take a question key array of any length, any number of answers per question, and as long as it is formatted correctly (it's a doozy of a formatting scheme, though, i'll tell you), it randomly gives you a question with the answers in a random order. I still don't have questions about java script built into it really, but this gives me the framework to do a lot of different things. Eventually, it would be cool to use the same program to feed questions about html and css.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,180 @@ var questionWheel = [
     ]
 ];
 
+var humanQuestions = [
+    [
+        "1 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "2 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "3 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "4 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "5 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "6 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "7 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "8 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "9 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "10 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "11 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "12 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "13 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "14 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "15 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "16 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "17 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "18 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ],
+    [
+        "19 is a question?",
+        [
+            ["haha yes", true],
+            ["fools rush in", false],
+            ["where wise men", false],
+            ["never go", false]
+        ]
+    ]
+]
+
 function shuffleArray(array) {
     var oldArray = [];
     for (i=0;i<array.length;i++){
@@ -35,25 +209,50 @@ function shuffleArray(array) {
     return newArray;
 }
 
+var myArray = ["a", "b", "c", "d", "e", "f"];
+
+var myNewArray = shuffleArray(myArray);
+
+console.log(myNewArray);
+
 function runGame(){
-    //reset display
-    display.innerHTML = "";
-    //display question
-    var question = document.createElement("h2");
-    question.textContent = questionWheel[queue][0];
-    display.appendChild(question);
-    //display answer buttons; we start at index 1 because index 0 is the question
-    for (var i = 1; i < questionWheel[queue].length; i++) {
-        var answer = document.createElement("button");
-        answer.textContent = questionWheel[queue][i][0];
-        answer.setAttribute("data-key", questionWheel[queue][i][1]);
-        display.appendChild(answer);
-        answer.addEventListener("click", function(){
-            if (this.getAttribute("data-key") == "true"){
-                console.log("hurray");
-            }
-        });
-    } 
+    function somefunction(questionList){
+    //first we randomize which questions show first
+    var listShuffled = shuffleArray(questionList);
+    //we'll start our current question at the beginning of our question array
+    var currentQuestion = 0
+    //this function displays our current question
+    function questionDisplay(){
+        display.innerHTML = "";
+        //display question
+        var question = document.createElement("h2");
+        question.textContent = listShuffled[currentQuestion][0];
+        display.appendChild(question);
+        //display answer buttons
+        //first lets shuffle those possible answers
+        var randAnswerArray = shuffleArray(listShuffled[currentQuestion][1])
+        //then we itterate through them, displaying them as well as creating event listeners for them
+        for (var i = 0; i < randAnswerArray.length; i++) {
+            var answer = document.createElement("button");
+            answer.textContent = randAnswerArray[i][0];
+            answer.setAttribute("data-key", randAnswerArray[i][1]);
+            display.appendChild(answer);
+            answer.addEventListener("click", function(){
+                if (this.getAttribute("data-key") == "true"){
+                    console.log("hurray");
+                    currentQuestion++;
+                    questionDisplay()
+                } else {
+                    console.log("oh no!")
+                    currentQuestion++;
+                    questionDisplay()
+                }
+            });
+        }
+    }
+    questionDisplay();
+    };
+    somefunction(humanQuestions);
 };
 
 start.addEventListener("click", runGame);
